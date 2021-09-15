cadeabde8c709c97a14d4578555a6ab9d46099bb
bemdev <mediaforge1@yandex.ru>
2021-09-15 14:46:52 +0300

    modify server and ref package.json

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 from libs.render import render
-from api.router import user_router, doc_router, assets_router
+from api.router import user_router, assets_router
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.gzip import GZipMiddleware
 
 
 app = FastAPI(docs_url='/api/docs/v1/')
 app.add_middleware(GZipMiddleware, minimum_size=100)
+app.mount("/public", StaticFiles(directory="public"), name="public")
 
 #-----------------------------------------------------------------------------#
 #--------------------------Assets endpoints with router-----------------------#
@@ -23,13 +25,12 @@ app.include_router(assets_router)
 def main_page():
     return StreamingResponse(render([]), media_type='text/html')
 
-@app.get("/news", include_in_schema=False)
-def news_page():
-    return StreamingResponse(render({ "view": "index" }), media_type='text/html')
+@app.get("/docs", include_in_schema=False)
+def docs_page():
+    return StreamingResponse(render({ "view": "docs" }), media_type='text/html')
 
 #-----------------------------------------------------------------------------#
 #-------------------------API endpoints with router---------------------------#
 #-----------------------------------------------------------------------------#
 
 app.include_router(user_router)
-app.include_router(doc_router)
